51367e716f14eead00746fb08b448c8defdda045
Daniel Rodrigues Ribeiro <danielrodriguesr@gmail.com>
2023-08-08 16:41:40 -0300

    Criados arquivos para deploy no Fly.io

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h)oz$-^tzn=bwfcupz%iij(_u#*i27kzp-at%ml&^!1shqs#zr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

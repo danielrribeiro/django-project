8abc86fb179f2b782defbcadb223ca7845a23ac3
Daniel Rodrigues Ribeiro <danielrodriguesr@gmail.com>
2023-08-23 12:50:20 -0300

    Configurados diretórios de arquivos estáticos e upload de amb-dev
    hold #42

--- pypro/settings.py
+++ pypro/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 from functools import partial
 from pathlib import Path
 from decouple import config, Csv
@@ -120,7 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# Development environment configuration
+
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

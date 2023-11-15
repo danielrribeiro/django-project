fb22c9fb8a6df66376977cd26dade7d5dbc55ee8
Daniel Rodrigues Ribeiro <danielrodriguesr@gmail.com>
2023-11-15 13:31:23 -0300

    Incluída url nomeada com uso de reverse e template tag url

--- pypro/urls.py
+++ pypro/urls.py
@@ -21,6 +21,6 @@ from pypro.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home, name='home'),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
